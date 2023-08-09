84ff34de165250443df1fa717341830f78ec5480
joao <=>
2023-08-08 22:14:02 -0300

    Alterações em aula

--- src/marca/marca.service.ts
+++ src/marca/marca.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { MARCA } from './marca.entity';
-import { RetornoCadastroDTO } from 'src/dto/retorno.dto';
+import { RetornoCadastroDTO, RetornoObjDTO } from 'src/dto/retorno.dto';
 import {v4 as uuid} from 'uuid';
 import { CriaMarcaDTO } from './dto/criaMarca.dto';
 
@@ -31,7 +31,7 @@ export class MarcaService {
     .catch((error) => {
       return <RetornoCadastroDTO>{
         id: "",
-        message: "Houve um erro ao cadastrar."
+        message: "Houve um erro ao cadastrar." + error.message
       };
     })
 
@@ -46,8 +46,58 @@ export class MarcaService {
     });
   }
 
-  async remover(id: string): Promise<void> {
-    const user = await this.localizarID(id);
-    await this.marcaRepository.remove(user);
+  listaNomes(): Promise<any[]> {
+    return this.marcaRepository.find({
+      select:{
+        NOME:true,
+      }
+    });
+  }
+
+  listaComForn(): Promise<any[]> {
+    var retorno = this.marcaRepository
+    .createQueryBuilder('marca')
+    .select('marca.nome','nome_marca')
+    .addSelect('pes_f.nome','nome_fornecedor')
+    .leftJoinAndSelect('for_marca', 'fm','fm.idmarca = marca.id')  
+    .leftJoinAndSelect('fornecedor', 'for','for.id = fm.idfornecedor')    
+    .leftJoinAndSelect('pessoa', 'pes_f','for.idpessoa = pes_f.id')     
+    .getRawMany();    
+    return retorno;    
+  }
+
+  async remover(id: string): Promise<RetornoObjDTO> {
+    const marca = await this.localizarID(id);
+    
+    return this.marcaRepository.remove(marca)
+    .then((result) => {
+      return <RetornoObjDTO>{
+        return: marca,
+        message: "Marca excluida!"
+      };
+    })
+    .catch((error) => {
+      return <RetornoObjDTO>{
+        return: marca,
+        message: "Houve um erro ao excluir." + error.message
+      };
+    });  
+  }
+
+  async alterar(id: string): Promise<RetornoCadastroDTO> {
+    const marca = await this.localizarID(id);
+    return this.marcaRepository.save(marca)
+    .then((result) => {
+      return <RetornoCadastroDTO>{
+        id: marca.ID,
+        message: "Marca alterada!"
+      };
+    })
+    .catch((error) => {
+      return <RetornoCadastroDTO>{
+        id: "",
+        message: "Houve um erro ao alterar." + error.message
+      };
+    });
   }
 }
